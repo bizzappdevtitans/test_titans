94cc1aa32f740a5c3d3fd8c8d986158b7638f0ab
Devam bhavsar <DEB@BAD.com>
2023-10-18 19:48:25 +0530

    [WIP][T00466]quick_procurement:Changed approach to fulfill requirements

--- quick_procurement/__manifest__.py
+++ quick_procurement/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "quick_procurement",
     "summary": """""",
-    "author": "bizzappdev",
+    "author": "BizzAppDev",
     "website": "http://bizzappdev.com",
     "category": "Uncategorized",
     "version": "15.0.1.0.0",
@@ -9,7 +9,6 @@
     "data": [
         "security/ir.model.access.csv",
         "wizard/product_procurement_purchase_order_wizard_views.xml",
-        "views/product_views.xml",
         "views/product_template_views.xml",
     ],
     "installable": True,
--- quick_procurement/wizard/product_procurement_purchase_order_wizard.py
+++ quick_procurement/wizard/product_procurement_purchase_order_wizard.py
@@ -1,5 +1,4 @@
-from odoo import models, fields, api
-
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -7,54 +6,118 @@ class ProductProcurementPurchaseOrder(models.TransientModel):
     _name = "product.procurement.purchase.order.wizard"
     _description = "Product Procurement Purchase Order"
 
-    scheduled_date = fields.Date(string="Scheduled Date")
-    product = fields.Many2one(
-        comodel_name="product.product",
-        default=lambda self: self._default_product_name(),
+    scheduled_date = fields.Date(required=True)
+    product_template = fields.Many2one(
+        comodel_name="product.template",
+        default=lambda self: self._default_product_template_name(),
     )
-    partner_id = fields.Many2one(comodel_name="res.partner", string="Vendor")
+    partner_id = fields.Many2one(
+        comodel_name="res.partner", string="Vendor", required=True
+    )
 
     def create_purchase_order_from_product(self):
         """function that will create purchase order from product with values from
         product #T00466"""
-        sale_orders = self.env["sale.order"].search(
-            [
-                ("order_line.product_id", "=", self.product.id),
-                ("date_order", ">", self.scheduled_date),
-                ("state", "=", "sale"),
-            ]
-        )
-        purchase_orders = self.env["purchase.order"].search(
-            [
-                ("order_line.product_id", "=", self.product.id),
-                ("date_planned", ">", self.scheduled_date),
-                ("state", "=", "purchase"),
-            ]
-        )
-        for orders in sale_orders and purchase_orders:
-            result = orders.order_line.product_uom_qty - orders.order_line.product_qty
-            if result < 0:
-                raise ValidationError("Cant Purchase more")
+        model = self.env.context.get("active_model")
+        product_id = self.env.context.get("active_id")
+        if model == "product.product":
+            sale_orders = self.env["sale.order"].search(
+                [
+                    (
+                        "order_line.product_id",
+                        "=",
+                        product_id,
+                    ),
+                    ("date_order", ">", self.scheduled_date),
+                    ("state", "=", "sale"),
+                ]
+            )
+            purchase_orders = self.env["purchase.order"].search(
+                [
+                    (
+                        "order_line.product_id",
+                        "=",
+                        product_id,
+                    ),
+                    ("date_planned", ">", self.scheduled_date),
+                    ("state", "=", "purchase"),
+                ]
+            )
+
+            total_sale_qty = sum(sale_orders.order_line.mapped("product_uom_qty"))
+            total_purchase_qty = sum(purchase_orders.order_line.mapped("product_qty"))
+            total_qty = total_sale_qty - total_purchase_qty
+
+            if total_qty < 0:
+                raise ValidationError(_("Cant Purchase more"))
             vals = {
                 "partner_id": self.partner_id.id,
                 "order_line": [
                     (
                         0,
                         0,
-                        {"product_id": self.product.id, "product_qty": result},
+                        {
+                            "product_id": product_id,
+                            "product_qty": total_qty,
+                        },
                     )
                 ],
             }
+            self.env["purchase.order"].create(vals)
+        else:
+            sale_orders = self.env["sale.order"].search(
+                [
+                    (
+                        "order_line.product_id",
+                        "in",
+                        self.product_template.product_variant_ids.ids,
+                    ),
+                    ("date_order", ">", self.scheduled_date),
+                    ("state", "=", "sale"),
+                ]
+            )
+            purchase_orders = self.env["purchase.order"].search(
+                [
+                    (
+                        "order_line.product_id",
+                        "in",
+                        self.product_template.product_variant_ids.ids,
+                    ),
+                    ("date_planned", ">", self.scheduled_date),
+                    ("state", "=", "purchase"),
+                ]
+            )
+            total_sale_qty = sum(sale_orders.order_line.mapped("product_uom_qty"))
+            total_purchase_qty = sum(purchase_orders.order_line.mapped("product_qty"))
+            total_qty = total_sale_qty - total_purchase_qty
+            if total_qty < 0:
+                raise ValidationError(_("Cant Purchase more"))
+            order_lines = []
+            for orders in sale_orders.order_line.product_id.ids:
+                order_lines.append(
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": orders,
+                            "product_qty": total_qty,
+                        },
+                    )
+                )
 
-        self.env["purchase.order"].create(vals)
+            self.env["purchase.order"].create(
+                {"partner_id": self.partner_id.id, "order_line": order_lines}
+            )
 
     def action_cancel(self):
         """Action for cancel button #T00466"""
-        pass
+        return
 
     @api.model
-    def _default_product_name(self):
-        """function to get default name of product in wizard #T00466"""
+    def _default_product_template_name(self):
+        """function to get current id of product in wizard #T00466"""
         context = dict(self._context) or {}
-        product = self.env["product.product"].browse(context.get("active_id", False))
-        return product and product.id
+        product_temp = self.env["product.template"].browse(
+            context.get("active_id", False)
+        )
+        return product_temp and product_temp.id
